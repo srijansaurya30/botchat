5e7bdc397604af339f74d4d70624b16879a9d9ff
Srijan Saurya <srijansaurya30@gmail.com>
2018-01-08 21:04:25 +0530

    Initial commit

---
+++ js/botchat.js
@@ -0,0 +1,8 @@
+function open_chatBox() {
+    $('#chatlaunch').fadeOut(500);
+    $('#chatBox').fadeIn(1000);
+}
+function close_chatBox() {
+    $('#chatBox').fadeOut(500);
+    $('#chatlaunch').fadeIn(1000);
+}
